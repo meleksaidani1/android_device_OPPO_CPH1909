a29c4bd2c634036c799e2d6447cb85a4486bdac3
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-10-30 14:09:08 -0700

    Update twrp_CPH1909.mk

--- twrp_CPH1909.mk
+++ twrp_CPH1909.mk
@@ -6,11 +6,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from CPH1909 device
 $(call inherit-product, device/oppo/CPH1909/device.mk)
